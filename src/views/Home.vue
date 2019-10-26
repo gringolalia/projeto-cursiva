889e78767856316dbe854a26ed1c2af47bedba08
Colin Brayton <gringolalia@gmail.com>
2019-10-26 16:24:20 -0300

    navigation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,42 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<template lang="pug">
+  i-container
+    i-row
+      i-column
+        h1
+          i.fas.fa-sun.fa-5x
+        h1  Good Morning, Midnight
+        p(style="text-align: left;") Cθ οδιο μθcιθσ ηισ, εθμ αλιι γραεcε δελιcατισσιμι ιδ, θτ παθλο qθαλισqθε εοσ. Ειρμοδ οπτιον qθαεστιο ιν μει. Cετεροσ αccθσαμθσ vιμ τε. Ενιμ cονσθλ ατομορθμ πρι ιν, νο ιγνοτα σθσcιπιαντθρ εοσ. Νονθμεσ προβατθσ vολθπτατιβθσ εαμ θτ, ωισι φιερεντ δελεcτθσ αδ vελ. Πριμισ εριπθιτ vθλπθτατε νο μει, τε qθι λεγιμθσ σθσcιπιτ περιcθλισ. Ερατ προπριαε ετ δθο, ιριθρε cορπορα μενανδρι εα εθμ.
+        i-button(variant="dark") Offprint
+        i-button(variant="primary") Imprint
+        i-button(variant="light") Footprint
+
+      i-column
+        h1
+          i.fas.fa-sun.fa-5x
+        h1  Good Morning, Midnight
+        p(style="text-align: left;") Cθ οδιο μθcιθσ ηισ, εθμ αλιι γραεcε δελιcατισσιμι ιδ, θτ παθλο qθαλισqθε εοσ. Ειρμοδ οπτιον qθαεστιο ιν μει. Cετεροσ αccθσαμθσ vιμ τε. Ενιμ cονσθλ ατομορθμ πρι ιν, νο ιγνοτα σθσcιπιαντθρ εοσ. Νονθμεσ προβατθσ vολθπτατιβθσ εαμ θτ, ωισι φιερεντ δελεcτθσ αδ vελ. Πριμισ εριπθιτ vθλπθτατε νο μει, τε qθι λεγιμθσ σθσcιπιτ περιcθλισ. Ερατ προπριαε ετ δθο, ιριθρε cορπορα μενανδρι εα εθμ.
+        i-button(variant="dark") Offprint
+        i-button(variant="primary") Imprint
+        i-button(variant="light") Footprint
+
+    i-row
+      i-column
+        h1
+          i.fas.fa-sun.fa-5x
+        h1  Good Morning, Midnight
+        p(style="text-align: left;") Cθ οδιο μθcιθσ ηισ, εθμ αλιι γραεcε δελιcατισσιμι ιδ, θτ παθλο qθαλισqθε εοσ. Ειρμοδ οπτιον qθαεστιο ιν μει. Cετεροσ αccθσαμθσ vιμ τε. Ενιμ cονσθλ ατομορθμ πρι ιν, νο ιγνοτα σθσcιπιαντθρ εοσ. Νονθμεσ προβατθσ vολθπτατιβθσ εαμ θτ, ωισι φιερεντ δελεcτθσ αδ vελ. Πριμισ εριπθιτ vθλπθτατε νο μει, τε qθι λεγιμθσ σθσcιπιτ περιcθλισ. Ερατ προπριαε ετ δθο, ιριθρε cορπορα μενανδρι εα εθμ.
+        i-button(variant="dark") Offprint
+        i-button(variant="primary") Imprint
+        i-button(variant="light") Footprint
+
+      i-column
+        h1
+          i.fas.fa-sun.fa-5x
+        h1  Good Morning, Midnight
+        p(style="text-align: left;") Cθ οδιο μθcιθσ ηισ, εθμ αλιι γραεcε δελιcατισσιμι ιδ, θτ παθλο qθαλισqθε εοσ. Ειρμοδ οπτιον qθαεστιο ιν μει. Cετεροσ αccθσαμθσ vιμ τε. Ενιμ cονσθλ ατομορθμ πρι ιν, νο ιγνοτα σθσcιπιαντθρ εοσ. Νονθμεσ προβατθσ vολθπτατιβθσ εαμ θτ, ωισι φιερεντ δελεcτθσ αδ vελ. Πριμισ εριπθιτ vθλπθτατε νο μει, τε qθι λεγιμθσ σθσcιπιτ περιcθλισ. Ερατ προπριαε ετ δθο, ιριθρε cορπορα μενανδρι εα εθμ.
+        i-button(variant="dark") Offprint
+        i-button(variant="primary") Imprint
+        i-button(variant="light") Footprint
 </template>
 
 <script lang="ts">
