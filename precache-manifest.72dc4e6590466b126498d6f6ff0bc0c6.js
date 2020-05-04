a4b215b79bfd80202bff3bf95bd7f4b8328c03b6
Colin Brayton <gringolalia@gmail.com>
2020-05-04 14:09:52 -0300

    deploy

---
+++ precache-manifest.72dc4e6590466b126498d6f6ff0bc0c6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e0d8c52da8de886901a",
+    "url": "/projeto-cursiva/js/about.a5a9adc5.js"
+  },
+  {
+    "revision": "033672e1b0e47efed230",
+    "url": "/projeto-cursiva/css/app.bd86faa2.css"
+  },
+  {
+    "revision": "033672e1b0e47efed230",
+    "url": "/projeto-cursiva/js/app.e33890c0.js"
+  },
+  {
+    "revision": "63bd5e0bcd7e9c61d71a",
+    "url": "/projeto-cursiva/css/chunk-vendors.45e5346e.css"
+  },
+  {
+    "revision": "63bd5e0bcd7e9c61d71a",
+    "url": "/projeto-cursiva/js/chunk-vendors.c009a150.js"
+  },
+  {
+    "revision": "58fd172793a8bbb613317c971d037804",
+    "url": "/projeto-cursiva/img/breugeltorre.58fd1727.jpg"
+  },
+  {
+    "revision": "9b6200320cf7c0cc32f17498808d2108",
+    "url": "/projeto-cursiva/img/ponteavmarinho.9b620032.jpg"
+  },
+  {
+    "revision": "92bc0ecfd4f279d9a41893461cb9b66d",
+    "url": "/projeto-cursiva/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/projeto-cursiva/robots.txt"
+  }
+];
